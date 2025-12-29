626c9ef31080041d0664082ad6afca560bf0a698
Abbasian.dev <mam-1371@outlook.com>
2025-12-29 20:12:22 +0330

    fix llm issues

--- lib/feature/llm/service/llm_service.dart
+++ lib/feature/llm/service/llm_service.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
-import 'package:flutter_gemma/flutter_gemma_mobile.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_gemma/flutter_gemma.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:logger/logger.dart';
 
